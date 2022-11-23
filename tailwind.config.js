96475aabc6e0e58a700b1e600fb2eed0b00a3844
tingtt <ting.taku@gmail.com>
2022-11-24 05:56:19 +0900

    chore: setup common packages

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+        'sans-serif',
+      ],
+    },
+  },
+  daisyui: {
+    themes: ['lofi', 'black'],
+    darkTheme: 'black',
+  },
+  plugins: [require('daisyui'), require('@tailwindcss/typography')],
+}
