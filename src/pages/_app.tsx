a852b3cbde93abbe24af933b89e797235a1e9dbb
tingtt <ting.taku@gmail.com>
2022-11-25 16:42:20 +0900

    feat: add `Head` to all page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>{`tingtt's portfolio`}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
