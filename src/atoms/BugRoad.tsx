bbdb22ca2c34d0e4f020b8154c1956557ee3f244
tingtt <ting.taku@gmail.com>
2022-11-27 18:22:46 +0900

    style(Hero): fix BugRoad style

--- src/atoms/BugRoad.tsx
+++ src/atoms/BugRoad.tsx
@@ -2,7 +2,7 @@ import { SvgBug } from './SvgBug'
 
 export const BugRoad = () => {
   return (
-    <div className='flex items-center bg-[#93E1E1] h-[1200px] md:h-[1600px] xl:h-[2000px] 2xl:h-[3000px] p-4'>
+    <div className='flex items-center bg-[#93E1E1] h-[1400px] md:h-[1600px] xl:h-[2000px] 2xl:h-[3000px] p-4'>
       {/* Bug icon */}
       <SvgBug />
     </div>
