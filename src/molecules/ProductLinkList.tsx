c29359a7cc938d10d5f3850df4213e9c9be1a43a
tingtt <ting.taku@gmail.com>
2022-12-08 17:22:33 +0900

    fix(Products): fix duplicated key

--- src/molecules/ProductLinkList.tsx
+++ src/molecules/ProductLinkList.tsx
@@ -10,19 +10,19 @@ export const ProductLinkList = ({ links }: { links: ProductLink[] }) => (
   <div className='flex flex-col gap-2 [&>*]:w-max'>
     {(links ?? []).map((l) => {
       if (l.type == 'github') {
-        return <LinkGitHub key={l.name} title={l.name} href={l.href} />
+        return <LinkGitHub key={l.href} title={l.name} href={l.href} />
       } else if (l.type == 'gitlab') {
         return (
-          <LinkGitLab key={l.name} title={l.name} href={l.href} blackAndWhite />
+          <LinkGitLab key={l.href} title={l.name} href={l.href} blackAndWhite />
         )
       } else if (l.type == 'web') {
-        return <LinkWeb key={l.name} title={l.name ?? ''} href={l.href} />
+        return <LinkWeb key={l.href} title={l.name ?? ''} href={l.href} />
       } else if (l.type == 'docs') {
-        return <LinkDocs key={l.name} title={l.name ?? ''} href={l.href} />
+        return <LinkDocs key={l.href} title={l.name ?? ''} href={l.href} />
       } else if (l.type == 'slide') {
-        return <LinkSlide key={l.name} title={l.name ?? ''} href={l.href} />
+        return <LinkSlide key={l.href} title={l.name ?? ''} href={l.href} />
       }
-      return <LinkDefault key={l.name} title={l.name ?? ''} href={l.href} />
+      return <LinkDefault key={l.href} title={l.name ?? ''} href={l.href} />
     })}
   </div>
 )
