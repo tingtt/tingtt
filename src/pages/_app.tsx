86d3173256b695882ef2d3bf07eaf02201ebbae7
tingtt <ting.taku@gmail.com>
2023-07-10 16:45:07 +0900

    refactor(seo): add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <Head>
           <title>{`tingtt's portfolio`}</title>
           <link rel='icon' href='/favicon.ico' />
+          <meta charSet='utf-8' />
+          <meta content="tingtt's portfolio." name='description' />
+          <meta property='og:title' content="tingtt's portfolio" />
+          <meta property='og:description' content="tingtt's portfolio." />
+          <meta property='og:type' content='website' />
+          <meta property='og:url' content='https://tingtt.jp/' />
+          <meta property='og:image' content='/prof_icon.webp' />
+          <meta property='og:site_name' content="tingtt's portfolio" />
+          <meta property='og:locale' content='ja_JP' />
         </Head>
         <Component {...pageProps} />
       </div>
